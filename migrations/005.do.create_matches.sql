d3d449b65c30f1a1479f8495f636ce433462728d
Trippy Leaf <95711738+TrippyLeaf@users.noreply.github.com>
2022-05-04 13:51:35 -0400

    Update 005.do.create_matches.sql

--- migrations/005.do.create_matches.sql
+++ migrations/005.do.create_matches.sql
@@ -3,16 +3,18 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 CREATE TABLE matches (
   match_id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
   match_start TIMESTAMP,
-  sport_id INTEGER NOT NULL,
-  FOREIGN KEY (sport_id) REFERENCES sports (sport_id),
   league_id INTEGER NOT NULL,
   FOREIGN KEY (league_id) REFERENCES leagues (league_id),
   home_team_id INTEGER NOT NULL,
   FOREIGN KEY (home_team_id) REFERENCES teams (team_id),
-  home_team_price REAL NOT NULL,
+  home_team_spread REAL NOT NULL,
+  home_team_moneyline REAL NOT NULL,
   away_team_id INTEGER NOT NULL,
   FOREIGN KEY (away_team_id)  REFERENCES teams (team_id),
-  away_team_price REAL NOT NULL,
+  away_team_spread REAL NOT NULL,
+  away_team_moneyline REAL NOT NULL,
+  total_over INTEGER NOT NULL,
+  total_under INTEGER NOT NULL,
   match_winner INTEGER DEFAULT NULL,
   FOREIGN KEY (match_winner) REFERENCES teams (team_id)
 );
