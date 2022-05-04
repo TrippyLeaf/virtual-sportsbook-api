17bcb61a8755968cfdcc3e8a07a40be6a9e8bd5a
Trippy Leaf <95711738+TrippyLeaf@users.noreply.github.com>
2022-05-04 18:18:38 -0400

    Update jwt-auth.js

--- src/middleware/jwt-auth.js
+++ src/middleware/jwt-auth.js
@@ -14,7 +14,7 @@ function requireAuth(req, res, next) {
   try {
     const payload = AuthService.verifyJwt(bearerToken)
 
-    AuthService.getUserWithUserName(
+    AuthService.getUserWithEmail(
       req.app.get('db'),
       payload.sub,
     )
