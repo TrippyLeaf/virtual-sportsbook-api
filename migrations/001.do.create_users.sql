3a49725231274bcc1178c4bdea5e745ffa6cadc8
Trippy Leaf <95711738+TrippyLeaf@users.noreply.github.com>
2022-05-04 13:35:44 -0400

    Update 001.do.create_users.sql

--- migrations/001.do.create_users.sql
+++ migrations/001.do.create_users.sql
@@ -1,8 +1,8 @@
 CREATE TABLE users (
   user_id SERIAL PRIMARY KEY,
-  user_name TEXT NOT NULL UNIQUE,
-  full_name TEXT NOT NULL,
+  email TEXT NOT NULL UNIQUE,
+  user_name TEXT NOT NULL,
   password TEXT NOT NULL,
-  user_balance INTEGER DEFAULT 1000,
+  user_balance INTEGER DEFAULT 0,
   date_created TIMESTAMP DEFAULT NOW()
 );
