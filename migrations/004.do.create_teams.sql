8e7953241384143c22058ae1f9d960cdc5eaf6cc
Trippy Leaf <95711738+TrippyLeaf@users.noreply.github.com>
2022-05-04 13:43:20 -0400

    Update 004.do.create_teams.sql

--- migrations/004.do.create_teams.sql
+++ migrations/004.do.create_teams.sql
@@ -1,5 +1,6 @@
 CREATE TABLE teams (
   team_id SERIAL PRIMARY KEY,
   league_id INTEGER NOT NULL,
-  team_name STRING NOT NULL
+  team_name TEXT NOT NULL UNIQUE,
+  FOREIGN KEY (league_id) REFERENCES leagues (league_id)
 );
